962c839a000dd175e7bcc8381042f45ab3e862b2
tmax818 <tylermaxwell661@gmail.com>
2023-05-02 11:28:28 -0700

    solution

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div>
       <PersonCard firstName={"Tyler"} lastName={"Maxwell"} age={39} hairColor={"bald"}/>
       <PersonCard firstName={"Jeff"} lastName={"Lee"} age={49} hairColor={"sandy blonde"}/>
-      <PersonCard firstName={"Mike"} lastName={"Kolora"} age={51} hairColor={"black"}/>
+      <PersonCard firstName={"Mike"} lastName={"Colora"} age={51} hairColor={"black"}/>
       <PersonCard firstName={"Mark"} lastName={"Watson"} age={51} hairColor={"bald"}/>
     </div>
   );
